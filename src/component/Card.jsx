8e1fe2be92fd78e2860b503259473920b12aaca4
shanemmay <shanemarvinmay@hotmail.com>
2018-09-01 14:37:27 -0500

    initial commit. basic card made, no logic, no redux)

---
+++ src/component/Card.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Card extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Card</h1>
+            </div>
+        );
+    }
+}
+
+export default Card;
