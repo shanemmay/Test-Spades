8aa6ad3ea26af78514f345e00c3d6fdc571c2876
shanemmay <shanemarvinmay@hotmail.com>
2018-09-01 18:48:47 -0500

    added deck and console logged the deck object

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import Card from './component/Card';
 import './App.css';
 
+import SpadesEngine from './logic/SpadesEngine';
+
+
 class App extends Component {
   render() {
+
+    console.log(SpadesEngine);
+    
     return (
       <div className="App">
         <Card />
---
+++ src/logic/SpadesEngine.js
@@ -0,0 +1,5 @@
+import deck from '../model/Deck';
+
+var d = deck;
+
+export default d;
